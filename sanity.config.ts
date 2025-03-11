23c47b1d832b9568647cafe4b98a0d482134291d
ELSOLRA <bardakasmiljo@gmail.com>
2025-03-11 15:08:11 +0100

    Initial commit with Next.js, Sanity, and Tailwind setup

---
+++ sanity.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemaTypes";
+
+export default defineConfig({
+  name: "personal-portfolio",
+  title: "Portfolio Next.js",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  apiVersion: "2025-03-11",
+  basePath: "/studio",
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+});
